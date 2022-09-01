5f5d4f013aa2a0de4ddcbe940c9cfbb539e70662
kbits000 <69712340+kbits000@users.noreply.github.com>
2022-09-01 17:25:05 +0300

    add then function

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ console.log("Hello World!");
 
 fetchedData = fetch ('https://api.imgflip.com/get_memes')
 console.log(fetchedData);
+
+fetchedData.then (
+    function(result) { console.log("Promise has been resolved") },
+    function(error) { /* handle an error */ }
+);
